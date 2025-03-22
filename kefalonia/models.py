25356501c4a511540521b4ae6412d1979e3491cb
dimtakos <dimitako@ihu.gr>
2025-03-22 19:34:28 +0200

    Create models.py ver1

--- kefalonia/models.py
+++ kefalonia/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+class Youtube(models.Model):
+    video = EmbedVideoField()
+
+class Place(models.Model):
+    place_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
