b1cfb813956aba0b747d0d934dd5639c38d05b9e
dimtakos <dimitako@ihu.gr>
2025-03-30 20:08:28 +0300

    Create urls.py ver2

--- Diplomatiki/urls.py
+++ Diplomatiki/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('kefalonia.urls')),
 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
