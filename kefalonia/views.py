28ed782fae0394761c72fdde4483e36cf7cfd7d9
dimtakos <dimitako@ihu.gr>
2025-03-30 20:40:28 +0300

    Create views.py ver2

--- kefalonia/views.py
+++ kefalonia/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
-    return render(request, 'kefalonia/main.html',{})
+    return render(request, 'kefalonia/main_page.html',{})
