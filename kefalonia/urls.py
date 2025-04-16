f8e69f9723bbae40e2180231ec9928e432ae410b
dimtakos <dimitako@ihu.gr>
2025-04-16 21:59:51 +0300

    Update urls.py ver3

--- kefalonia/urls.py
+++ kefalonia/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('destinations.html', views.destinations, name='destinations'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
