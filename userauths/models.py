55b9cb329a711e53dbf9044c59ac4bf6088a584b
dimtakos <dimitako@ihu.gr>
2025-05-23 10:17:56 +0300

    Update db.sqlite3 ver3

--- userauths/models.py
+++ userauths/models.py
@@ -2,7 +2,7 @@ import string
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.db.models.signals import post_save
 from shortuuid.django_fields import ShortUUIDField
 # Create your models here.
 
@@ -32,7 +32,7 @@ class User(AbstractUser):
 
     otp = models.CharField(max_length=100, null=True, blank=True)
 
-    USERNAME_FIELD = ['email']
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
     def __str__(self):
@@ -40,7 +40,7 @@ class User(AbstractUser):
 
 
 class Profile(models.Model):
-    pid = ShortUUIDField(Length=7, max_length=25, alphabets="abcdefghijklmnopqrstuvwxyz123")
+    pid = ShortUUIDField(length=7, max_length=25, alphabet="abcdefghijklmnopqrstuvwxyz123")
     image = models.FileField(upload_to=user_directory_path, default="default.jpg", null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=500, null=True, blank=True)
@@ -52,4 +52,32 @@ class Profile(models.Model):
     state = models.CharField(max_length=100, null=True, blank=True)
     address = models.CharField(max_length=1000, null=True, blank=True)
 
-    identity_type = models.CharField(max_length=20, choices=IDENTITY_TYPE, null=True, blank=True)
+    identity_type = models.CharField(max_length=200, choices=IDENTITY_TYPE, null=True, blank=True)
+    identity_image = models.FileField(upload_to=user_directory_path, default="id.jpg", null=True, blank=True)
+
+    facebook = models.URLField(max_length=100,blank=True)
+    twitter = models.URLField(max_length=100, blank=True)
+
+    wallet = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    verified = models.BooleanField(default=False)
+
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        if self.full_name:
+            return f"{self.full_name}"
+        else:
+            return f"{self.user.username}"
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
